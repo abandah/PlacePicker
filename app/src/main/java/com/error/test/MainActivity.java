753c6d4ab1523f4c1059a61c575df89214b20212
mohammad <m3bndah@gmail.com>
2020-07-21 17:59:23 +0300

    as

---
+++ app/src/main/java/com/error/test/MainActivity.java
@@ -0,0 +1,22 @@
+package com.error.test;
+
+import androidx.appcompat.app.AppCompatActivity;
+import android.os.Bundle;
+import android.view.View;
+
+public class MainActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+        findViewById(R.id.button).setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                int i[] = new int[2];
+                int y = i[3];
+            }
+        });
+
+    }
+}
